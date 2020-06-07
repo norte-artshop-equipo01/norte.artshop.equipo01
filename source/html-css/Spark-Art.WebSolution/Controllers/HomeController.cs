7bc1cf88bca8dc8438297aa4f20f1e54d467c18e
Emmanuel Leardi <emmaleardi@hotmail.com>
2020-06-06 21:13:56 -0300

    Agregado de imagenes, recorrido de la lsita para la Galeria

--- source/html-css/Spark-Art.WebSolution/Controllers/HomeController.cs
+++ source/html-css/Spark-Art.WebSolution/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using art_shop_core.EntityFramework;
+using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services;
 
 namespace Lppa.WebSite.Controllers
 {
     public class HomeController : Controller
     {
+        List<Product> obrasdev = new List<Product>();
+
         public ActionResult Index()
         {
             return View();
@@ -25,13 +31,20 @@ namespace Lppa.WebSite.Controllers
         public ActionResult Galeria()
         {
             ViewBag.Message = "La página de descripción de su aplicación.";
-            return View();
+           /* test();*/
+            
+            EntityFrameworkConnection conn = new EntityFrameworkConnection("art-shop-model", "edu-spark-art", @"LOCALHOST\SQLEXPRESS");
+            List<Product> obrasdev = conn.Find<Product>(product => product.Id > 0);
+            return View(obrasdev);
+            // return PartialView(obrasdev);
+            
         }
 
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Su página de contacto.";
+            
             return View();
         }
 
@@ -40,5 +53,19 @@ namespace Lppa.WebSite.Controllers
         {
             return RedirectToAction("Index");
         }
+
+        [WebMethod]
+        public List<Product> Test()
+        {
+            var efConn = new EntityFrameworkConnection("art-shop-model", "edu-spark-art", @"LOCALHOST\SQLEXPRESS");
+            
+            var obras = efConn.Find<Product>(product => product.Id > 0);
+            foreach (var item in obras)
+            {
+                var file = item.Image;
+            }
+            return  obras;
+        }
+        
     }
 }
