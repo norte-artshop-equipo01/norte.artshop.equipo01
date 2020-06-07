042b104ef35f7fef30ca1cf38a330e9d6cd8ab1c
Victor Grycuk <victor@grycuk.net>
2020-06-07 17:27:06 -0300

    Fix context

--- source/business/art-shop-core/art-shop-core/EntityFramework/art-shop-model.Context.cs
+++ source/business/art-shop-core/art-shop-core/EntityFramework/art-shop-model.Context.cs
@@ -15,8 +15,8 @@ namespace art_shop_core.EntityFramework
     
     public partial class Entities : DbContext
     {
-        public Entities(string v)
-            : base("name=Entities")
+        public Entities(string connectionString)
+            : base(connectionString)
         {
         }
     
